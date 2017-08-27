e264c48226e0537613f1f36a3246be72dc637cda
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2017-08-27 18:27:30 -0300

    Added React Router v4

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Provider } from '../lib/react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from './App'
 
 const Root = ({ store }) =>
   <Provider store={store}>
-    <App/>
+    <Router>
+      <Route exact path='/' component={App} />
+    </Router>
   </Provider>
 
 export default Root
