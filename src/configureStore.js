1fde6d25d9730ad92993b3541ea91b69ce799d29
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2017-08-27 21:52:32 -0300

    Wrapping dispatch() with Log Actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -11,6 +11,28 @@ const configureStore = () => {
     persistedState/*,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //ReduxDevTools => just use it with the original Redux */
   )
+
+  const addLoggingToDispatch = store => {
+    const rawDispatch = store.dispatch.bind(store)
+    if (!console.group) {
+      return rawDispatch
+    }
+  
+    return action => {
+      console.group(action.type)
+      console.log('%c prev state', 'color: grey', store.getState())
+      console.log('%c action', 'color: yellow', action)
+      const returnValue = rawDispatch(action)
+      console.log('%c next state', 'color: green', store.getState())
+      console.groupEnd(action.type)
+      return returnValue
+    }
+  }
+  
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store)
+  }
+
   store.subscribe(throttle(() => {
     saveState(KEY, {
       todos: store.getState().todos
