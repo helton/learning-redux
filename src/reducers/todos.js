2a539ae40ff7820579138649c0393c809c426bff
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2017-08-27 21:41:55 -0300

    Normalizing the state shape

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,49 +1,48 @@
-const todos = (state = [], action) => {
+import { combineReducers } from '../lib/redux'
+import todo from './todo'
+
+const byId = (state = {}, action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ]
     case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action))
+    return {
+      ...state,
+      [action.id]: todo(state[action.id], action)
+    }
     default:
       return state
   }
 }
 
-const todo = (state, action) => {
+const allIds = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      }
-    case 'TOGGLE_TODO':
-      if (state.id !== action.id)
-        return state
-      return { 
-        ...state, 
-        completed: !state.completed
-      }
+      return [...state, action.id]
     default:
       return state
   }
 }
+
+const todos = combineReducers({
+  byId,
+  allIds
+})
 
 export default todos
 
+const getAllTodos = state => state.allIds.map(id => state.byId[id])
+
 //selectors
 export const getVisibleTodos = (state, filter) => {
+  const allTodos = getAllTodos(state)
   switch (filter) {
     case 'all':
-      return state
+      return allTodos
     case 'active':
-      return state.filter(t => !t.completed)
+      return allTodos.filter(t => !t.completed)
     case 'completed':
-      return state.filter(t => t.completed)
+      return allTodos.filter(t => t.completed)
     default:
-      return state
+      throw new Error(`Unknown filter ${filter}.`)
   }
 }
