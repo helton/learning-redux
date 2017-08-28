5e17535c5902972ac3fb1ea16c65a9f07b502e6f
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2017-08-28 19:25:28 -0300

    The Middleware Chain

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,42 +1,43 @@
 import { createStore } from './lib/redux'
 import todoApp from './reducers' 
 
+const logger = store => next => {
+  if (!console.group) {
+    return next
+  }
+
+  return action => {
+    console.group(action.type)
+    console.log('%cprev state', 'color: grey', store.getState())
+    console.log('%caction', 'color: yellow', action)
+    const returnValue = next(action)
+    console.log('%cnext state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+    return returnValue
+  }
+}
+
+const promise = store => next => async action => next(typeof action.then === 'function' ? await action : action)
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware =>
+    store.dispatch = middleware(store)(store.dispatch.bind(store))
+  )
+}
+
 const configureStore = () => {
   const store = createStore(
     todoApp/*,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //ReduxDevTools => just use it with the original Redux */
   )
 
-  const addLoggingToDispatch = store => {
-    const rawDispatch = store.dispatch.bind(store)
-    if (!console.group) {
-      return rawDispatch
-    }
-  
-    return action => {
-      console.group(action.type)
-      console.log('%cprev state', 'color: grey', store.getState())
-      console.log('%caction', 'color: yellow', action)
-      const returnValue = rawDispatch(action)
-      console.log('%cnext state', 'color: green', store.getState())
-      console.groupEnd(action.type)
-      return returnValue
-    }
-  }
-
-  const addPromiseSupportToDispatch = store => {
-    const rawDispatch = store.dispatch.bind(this)
-    return async action => {
-      const actionValue = typeof action.then === 'function' ? await action : action
-      return rawDispatch(actionValue)
-    }
-  }
+  const middlewares = [promise]
   
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store)
+    middlewares.push(logger)
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store)
+  wrapDispatchWithMiddlewares(store, middlewares)
 
   return store
 }
